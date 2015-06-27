ee473c13dff6879fceb62da2e3319af9e35ef4f1
Manoj Pandey <manojpandey1996@gmail.com>
2015-06-28 04:11:30 +0430

    crawling pagings

--- stack/spiders/stack_crawler.py
+++ stack/spiders/stack_crawler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -9,15 +8,25 @@ from stack.items import StackItem
 class StackCrawlerSpider(CrawlSpider):
     name = 'stack_crawler'
     allowed_domains = ['stackoverflow.com']
-    start_urls = ['http://www.stackoverflow.com/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    # start_urls = ['http://www.stackoverflow.com/']
+    start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=votes']
+
+    # rules = (
+    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    # )
+
+    rules = [
+        Rule(LinkExtractor(allow=r'questions\?page=[0-9]&sort=votes'), callback='parse_item', follow=True)
+    ]
 
     def parse_item(self, response):
-        i = StackItem()
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        questions = response.xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StackItem()
+            item['url'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            yield item
